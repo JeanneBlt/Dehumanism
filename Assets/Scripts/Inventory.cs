9f08161bdb0b74c19b5a820fdd2f2526c16dac53
jeanneblt <jeanne.boulet@edu.esiee.fr>
2024-10-10 16:14:07 +0200

    adding visual for inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField]
     private List<ItemData> content = new List<ItemData>();
 
+    [SerializeField]
+    private GameObject inventoryPanel;
+
+    [SerializeField]
+    private Transform inventorySlotsParent;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+        }
+
+        RefreshContent();
+    }
+
     public void AddItem(ItemData item)
     {
         content.Add(item);
     }
+
+    public void CloseInventory()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    private void RefreshContent()
+    {
+        for (int i = 0; i < content.Count; i++)
+        {
+            inventorySlotsParent.GetChild(i).GetChild(0).GetComponent<Image>().sprite = content[i].visual;
+        }
+    }
+
 }
